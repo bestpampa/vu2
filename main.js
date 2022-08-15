bc6c362b36330d7922ebb4230880947f968a6ea4
bestpamp <aguspitarque@hotmail.com>
2022-08-14 21:00:30 -0300

    vue 2 lesson 2

---
+++ main.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        subproduct: 'Medias',
+        description: 'A pair of warm, fuzzy socks',
+        imagen: './assets/greenSocks.jpg',
+        url: 'https://vuemastery.com'
+    }
+})
