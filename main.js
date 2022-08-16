3e49e5fc4b758607117727ca3cdc819c5b254a8a
bestpamp <aguspitarque@hotmail.com>
2022-08-15 22:38:51 -0300

    Lesson 5

--- main.js
+++ main.js
@@ -5,6 +5,21 @@ var app = new Vue({
         subproduct: 'Medias',
         description: 'A pair of warm, fuzzy socks',
         imagen: './assets/greenSocks.jpg',
-        url: 'https://vuemastery.com'
+        url: 'https://vuemastery.com',
+        inStock: true,
+        inventory: 100,
+        onSale: true,
+        detalles: ["80% algodon", "20% polyester", "Gender neutral"],
+        talles: ["Large", "Medium", "Small"],
+        variantes: [
+            {
+                variantId: 2234,
+                variantColor: "green" 
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue" 
+            }
+        ]
     }
 })
