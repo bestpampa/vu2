4a045722af582571e7b528d73156c4bb71285852
bestpamp <aguspitarque@hotmail.com>
2022-09-03 22:37:41 -0300

    v2

--- main.js
+++ main.js
@@ -1,25 +1,34 @@
+//When inStock is false, bind a class to the “Out of Stock” p tag that adds  text-decoration: line-through to that element.
+
 var app = new Vue({
     el: '#app',
     data: {
-        product: 'Socks',
-        subproduct: 'Medias',
-        description: 'A pair of warm, fuzzy socks',
-        imagen: './assets/greenSocks.jpg',
-        url: 'https://vuemastery.com',
-        inStock: true,
-        inventory: 100,
-        onSale: true,
-        detalles: ["80% algodon", "20% polyester", "Gender neutral"],
-        talles: ["Large", "Medium", "Small"],
-        variantes: [
-            {
-                variantId: 2234,
-                variantColor: "green" 
-            },
-            {
-                variantId: 2235,
-                variantColor: "blue" 
-            }
-        ]
+      product: 'Socks',
+      image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+      inStock: true,
+      details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+      variants: [
+        {
+          variantId: 2234,
+          variantColor: 'green',
+          variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg'
+        },
+        {
+          variantId: 2235,
+          variantColor: 'blue',
+          variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg'
+        }
+      ],
+      cart: 0
+    },
+    methods: {
+      addToCart() {
+        this.cart += 1
+      },
+      updateProduct(variantImage) {
+        this.image = variantImage
+      }
     }
-})
+  })
+  
+  
